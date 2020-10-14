f66684576e36707720788b0b65a36a3a80f800d5
YOSSI263 <46202069+YOSSI263@users.noreply.github.com>
2020-10-14 23:12:47 +0300

    Create panel.php

---
+++ panel.php
@@ -0,0 +1,55 @@
+<body class="body">
+  <div class="panel">
+    <div class="text">שווה לבקר<br>‍</div>
+    <a href="https://tchumim.com/" target="_blank" class="w-inline-block"><img src="https://mitmachim.top/assets/uploads/files/1602702078674-tchumim.png" alt="" class="img"></a>
+    <a href="https://www.prog.co.il/" target="_blank" class="w-inline-block"><img src="https://mitmachim.top/assets/uploads/files/1602702061746-prog.png" alt="" class="img"></a>
+    <a href="https://www.tora-forum.co.il/" target="_blank" class="w-inline-block"><img src="https://www.tora-forum.co.il/ext/tatiana5/favicon/favicon.ico" alt="" class="img"></a>
+    <a href="https://forum-office.ml/" target="_blank" class="w-inline-block"><img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Microsoft_Office_logo_%282019%E2%80%93present%29.svg" alt="" class="img"></a>
+    <a href="https://xn--9dbazrsfz.com/"target="_blank" class="w-inline-block"><img src="https://mitmachim.top/assets/uploads/files/1602701994040-mk.png" alt="" class="img"></a>
+    <a href="https://f2.freeivr.co.il/"target="_blank" class="w-inline-block"><img src="https://mitmachim.top/assets/uploads/files/1602704764319-%D7%99%D7%9E%D7%95%D7%AA.png" alt="" class="img"></a></div>
+
+
+<style>
+
+.body {
+  direction: ltr;
+  text-align: center;
+}
+
+.panel {
+  position: fixed;
+  display: flex;
+  width: 60px;
+  height: auto;
+  left: 0px;
+  bottom: 80px;
+  padding-top: 12px;
+  padding-bottom: 8px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-top-right-radius:20px;
+  border-bottom-right-radius: 20px;
+  background-color: #e4e8ff;
+}
+
+.text {
+  display: block;
+  margin-right: auto;
+  margin-bottom: 6px;
+  margin-left: auto;
+  @import url('<link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Varela+Round&display=swap" rel="stylesheet">');
+  font-family: 'Varela Round', sans-serif;
+  line-height: 18px;
+  font-weight: 700;
+  text-align: center;
+}
+
+.img {
+  width: 37px;
+  margin-bottom: 15px;
+}
+
+</style>
+
+</body>
